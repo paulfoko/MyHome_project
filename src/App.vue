950a97b0b93bc95476f88e782c8369c585dde4b7
Danielkinkeu <kinkeufranck@gmail.com>
2023-03-08 16:48:08 +0100

    login et signUp
    authentification

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,15 @@
 <template>
   <RouterView />
 </template>
-
+<script>
+import SignUp from './components/SignUp.vue'
+import login from './components/login.vue'
+export default {
+  name: 'App',
+  components: {
+    SignUp,
+    login
+  }
+}
+</script>
 <style scoped></style>
